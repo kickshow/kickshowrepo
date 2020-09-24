66c092e5abca9f4c43a524f478d64ee7b3defce2
Pablo Juan Villanueva <pablo.juan@jsv.es>
2020-09-24 10:09:48 +0200

    Delete folders bin and obj

---
+++ kickshowrepo/Views/ViewsLogin/StartPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kickshowrepo.Views.ViewsLogin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterNav_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+
+        private void LoginNav_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+
+    }
+}
